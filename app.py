7c696b85babbc2f44096495d70c593bf21bdc0cc
andy5wt3t <Maidaqs@vivaldi.net>
2026-01-31 16:32:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ PROJECT_URL = os.environ.get('PROJECT_URL', '')        ,
 AUTO_ACCESS = os.environ.get('AUTO_ACCESS', 'false').lower() == 'true'  
 FILE_PATH = os.environ.get('FILE_PATH', './.cache')   
 SUB_PATH = os.environ.get('SUB_PATH', 'sub')           
-UUID = os.environ.get('UUID', '01010101-0101-0101-0101-010101010101')  
+UUID = os.environ.get('UUID', '7ed63d16-81b9-44df-846d-336301f89e1b')  
 NEZHA_SERVER = os.environ.get('NEZHA_SERVER', '')     
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')          
 NEZHA_KEY = os.environ.get('NEZHA_KEY', '')           
